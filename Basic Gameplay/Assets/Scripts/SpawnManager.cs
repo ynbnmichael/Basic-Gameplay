f6c6da97acde5ca4731bb3df082bd3b42da21450
easdmichael <kinaram@roverkids.org>
2024-12-09 14:13:53 -0500

    part of 2.4 complete

--- Basic Gameplay/Assets/Scripts/SpawnManager.cs
+++ Basic Gameplay/Assets/Scripts/SpawnManager.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
@@ -5,6 +6,9 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float espawnInterval = 1.5f;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,6 +19,13 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+  
+
+
+    }
+
+    void SpawnRandomAnimal()
+    {
         if (Input.GetKeyDown(KeyCode.S))
         {
             int animalIndex = Random.Range(0, animalPrefabs.Length);
@@ -22,7 +33,14 @@ public class SpawnManager : MonoBehaviour
 
             Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
         }
-
-
     }
+
+
+
+
+
+
+
 }
+
+
